08dc5a2ba1795c12f35c09cee461707d93af502a
ayushrskiaa <ayushkumar2263@iiitmanipur.ac.in>
2025-10-29 09:41:07 +0530

    update

--- RailSafetyApp/app/src/main/java/com/example/myapplication2/services/NotificationService.kt
+++ RailSafetyApp/app/src/main/java/com/example/myapplication2/services/NotificationService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
@@ -37,7 +38,11 @@ class NotificationService : Service() {
             .setSmallIcon(R.drawable.ic_train)
             .build()
 
-        startForeground(1, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        } else {
+            startForeground(1, notification)
+        }
         Log.d("NotificationService", "Service Started in foreground.")
         return START_STICKY
     }
